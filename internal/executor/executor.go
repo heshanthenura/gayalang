2d7674b7048e0e9a7c857b1dfdf7c559c7e3d083
Heshan Thenura Kariyawasam <heshanthenura@protonmail.com>
2026-01-05 21:41:14 +0530

    feat: add colors

--- internal/executor/executor.go
+++ internal/executor/executor.go
@@ -6,20 +6,24 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/fatih/color"
 	"github.com/heshanthenura/gayalang/internal/ast"
 )
 
 type Context map[string]string
 
 func ExecuteProgram(prog *ast.Program) Context {
+	red := color.New(color.FgRed).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
+
 	ctx := Context{}
 
 	for _, req := range prog.Requests {
 		status, body := executeRequest(req, ctx)
 		if req.Expect.Status != 0 && status != req.Expect.Status {
-			fmt.Printf("Request %s failed: expected %d but got %d\n", req.Name, req.Expect.Status, status)
+			fmt.Printf("%s Request %s failed: expected %d but got %d\n", red("[FAILED]"), req.Name, req.Expect.Status, status)
 		} else {
-			fmt.Printf("Request %s succeeded: %d\n", req.Name, status)
+			fmt.Printf("%s Request %s succeeded: %d\n", green("[PASS]  "), req.Name, status)
 		}
 
 		if req.SaveVar != "" {
